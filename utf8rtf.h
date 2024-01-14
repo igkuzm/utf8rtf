a934af988bb71229293e99df851c6d53c214553d
Igor V. Sementsov <ig.kuzm@gmail.com>
2024-01-14 21:05:48 +1000

    fix

--- utf8rtf.h
+++ utf8rtf.h
@@ -2,7 +2,7 @@
  * File              : utf8rtf.h
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 06.09.2021
- * Last Modified Date: 12.09.2023
+ * Last Modified Date: 14.01.2024
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
 
@@ -26,265 +26,231 @@ static int utf8rtf_decode();
  */
 static int utf8rtf_encode();
 
-
-
 /*
  * IMP
  */
 
-void 
-_utf8_to_utf32(
-		int count, 
-		uint8_t *utf8_char, 
-		void * user_data, 
-		void (*callback)(void * user_data, uint32_t utf32_char))
-{
-	uint8_t *ptr = utf8_char; //pointer (do not change value)
-	uint32_t utf32_char = 0;
-
-	switch (count) {
-		case 4: {
-			//take last 3 bit from first char
-			uint32_t byte0 = (*ptr++ & 0x7) << 18;   //0b00000111	
-			
-			//take last 6 bit from second char
-			uint32_t byte1 = (*ptr++ & 0x3f) << 12;  //0b00111111	
-			
-			//take last 6 bit from third char
-			uint32_t byte2 = (*ptr++ & 0x3f) << 6;   //0b00111111	
-			
-			//take last 6 bit from forth char
-			uint32_t byte3 = *ptr++ & 0x3f;          //0b00111111	
-			
-			utf32_char = (byte0 | byte1 | byte2 | byte3);					
-
-			break;
-		} 
-
-		case 3: {
-			//take last 4 bit from first char
-			uint32_t byte0 = (*ptr++ & 0xf) << 12;  //0b00001111	
-			
-			//take last 6 bit from second char
-			uint32_t byte1 = (*ptr++ & 0x3f) << 6;  //0b00111111	
-			
-			//take last 6 bit from third char
-			uint32_t byte2 = *ptr++ & 0x3f;         //0b00111111
-
-			utf32_char = (byte0 | byte1 | byte2);
-
-			break;
-		} 
-
-		case 2: {
-			//take last 5 bit from first char
-			uint32_t byte0 = (*ptr++ & 0x1f) << 6;  //0b00011111
-			
-			//take last 6 bit from second char
-			uint32_t byte1 = *ptr++ & 0x3f;	        //0b00111111
-
-			utf32_char = (byte0 | byte1);
-
-			break;
-		} 
-
-		case 1: {
-			utf32_char = *ptr++;
-
-			break;
-		} 				
-
-		default:
-			break;
-				
+/* convert utf32 char to utf8 multybite char array and return number of bytes */ 
+static int _utf8rtf_c32tomb(char s[6], const uint32_t c32){
+	int i = 0;
+	if (c32 <= 0x7F) {
+		// Plain single-byte ASCII.
+		s[i++] = (char) c32;
 	}
-	if (callback)
-		if(utf32_char)
-			callback(user_data, utf32_char);
+	else if (c32 <= 0x7FF) {
+		// Two bytes.
+		s[i++] = 0xC0 |  (c32 >> 6);
+		s[i++] = 0x80 | ((c32 >> 0) & 0x3F);
+	}
+	else if (c32 <= 0xFFFF) {
+		// Three bytes.
+		s[i++] = 0xE0 |  (c32 >> 12);
+		s[i++] = 0x80 | ((c32 >> 6) & 0x3F);
+		s[i++] = 0x80 | ((c32 >> 0) & 0x3F);
+	}
+	else if (c32 <= 0x1FFFFF) {
+		// Four bytes.
+		s[i++] = 0xF0 |  (c32 >> 18);
+		s[i++] = 0x80 | ((c32 >> 12) & 0x3F);
+		s[i++] = 0x80 | ((c32 >> 6)  & 0x3F);
+		s[i++] = 0x80 | ((c32 >> 0)  & 0x3F);
+	}
+	else if (c32 <= 0x3FFFFFF) {
+		// Five bytes.
+		s[i++] = 0xF8 |  (c32 >> 24);
+		s[i++] = 0x80 | ((c32 >> 18) & 0x3F);
+		s[i++] = 0x80 | ((c32 >> 12) & 0x3F);
+		s[i++] = 0x80 | ((c32 >> 6)  & 0x3F);
+		s[i++] = 0x80 | ((c32 >> 0)  & 0x3F);
+	}
+	else if (c32 <= 0x7FFFFFFF) {
+		// Six bytes.
+		s[i++] = 0xFC |  (c32 >> 30);
+		s[i++] = 0x80 | ((c32 >> 24) & 0x3F);
+		s[i++] = 0x80 | ((c32 >> 18) & 0x3F);
+		s[i++] = 0x80 | ((c32 >> 12) & 0x3F);
+		s[i++] = 0x80 | ((c32 >> 6)  & 0x3F);
+		s[i++] = 0x80 | ((c32 >> 0)  & 0x3F);
+	}
+	else{
+		// Invalid char; don't encode anything.
+	}	
+
+	return i;
 }
 
-void
-_utf32_to_utf8(
-		uint32_t utf32_char,
-		void * user_data,
-		int (*callback)(void * user_data, uint8_t utf8_char)		
+/* convert utf8 multybite null-terminated string to utf32 null-terminated string 
+ * and return it's len */ 
+static size_t _utf8rtf_mbtoc32(uint32_t *s32, const char *s){
+	char *ptr = (char *)s;
+	size_t i = 0;
+	while (*ptr){
+		uint8_t c = *ptr;
+		if (c >= 252){/* 6-bytes */
+			s32[i]  = (*ptr++ & 0x1)  << 30;  // 0b00000001
+			s32[i] |= (*ptr++ & 0x3F) << 24;  // 0b00111111	
+			s32[i] |= (*ptr++ & 0x3F) << 18;  // 0b00111111
+			s32[i] |= (*ptr++ & 0x3F) << 12;  // 0b00111111
+			s32[i] |= (*ptr++ & 0x3F) << 6;   // 0b00111111
+			s32[i] |=  *ptr++ & 0x3F;         // 0b00111111
+			i++;
+		} 
+		else if (c >= 248){/* 5-bytes */
+			s32[i]  = (*ptr++ & 0x3)  << 24;  // 0b00000011
+			s32[i] |= (*ptr++ & 0x3F) << 18;  // 0b00111111
+			s32[i] |= (*ptr++ & 0x3F) << 12;  // 0b00111111
+			s32[i] |= (*ptr++ & 0x3F) << 6;   // 0b00111111
+			s32[i] |=  *ptr++ & 0x3F;         // 0b00111111
+			i++;
+		}
+		else if (c >= 240){/* 4-bytes */
+			s32[i]  = (*ptr++ & 0x7)  << 18;  // 0b00000111
+			s32[i] |= (*ptr++ & 0x3F) << 12;  // 0b00111111
+			s32[i] |= (*ptr++ & 0x3F) << 6;   // 0b00111111
+			s32[i] |=  *ptr++ & 0x3F;         // 0b00111111
+			i++;
+		} 
+		else if (c >= 224){/* 3-bytes */
+			s32[i]  = (*ptr++ & 0xF)  << 12;  // 0b00001111
+			s32[i] |= (*ptr++ & 0x3F) << 6;   // 0b00111111
+			s32[i] |=  *ptr++ & 0x3F;         // 0b00111111
+			i++;                
+		}
+		else if (c >= 192){/* 2-bytes */
+			s32[i]  = (*ptr++ & 0x1F) << 6;   // 0b00011111
+			s32[i] |=  *ptr++ & 0x3F;         // 0b00111111 
+			i++; 
+		} 
+		else{/* 1-byte */
+			s32[i++] = *ptr++;
+		} 
+	}
+
+	// null-terminate string
+	s32[i] = 0;
+	return i;
+}	
+
+static int _utf8rtf_isinword(int ch)
+{
+	if ( 	
+		ch == '\n' ||
+		ch == '\r' ||
+		ch == ' '  ||
+		ch == '\t' ||
+		ch == '}'  ||
+		ch == '{'  ||
+		ch == '\\'
 		)
-{
-	//more than 00000000 00000100 00000000 00000000
-	if (utf32_char > 0x040000){ //4-byte
-		uint8_t utf8_char;
-
-		//get first byte - first 3 bit 00000000 00011100 00000000 00000000
-		//and mask with 11110000 
-		utf8_char = ((utf32_char & 0x1C0000) >> 18) | 0xF0;
-		if (callback)
-			if(callback(user_data, utf8_char))
-				return;
-
-		//get second - 00000000 00000011 11110000 00000000
-		//and mask with 10000000 
-		utf8_char = ((utf32_char & 0x03F000) >> 12) | 0x80;
-		if (callback)
-			if(callback(user_data, utf8_char))
-				return;
-		
-		//get third - 00000000 00000000 00001111 11000000
-		//and mask with 10000000 
-		utf8_char = ((utf32_char & 0x0FC0) >> 6 )   | 0x80;
-		if (callback)
-			if(callback(user_data, utf8_char))
-				return;
-
-		//get last - 00000000 00000000 00000000 00111111
-		//and mask with 10000000 
-		utf8_char = ( utf32_char & 0x3F)            | 0x80;
-		if (callback)
-			if(callback(user_data, utf8_char))
-				return;
-	}
-	//more than 00000000 00000000 00010000 00000000
-	else if (utf32_char > 0x1000){ //3-byte
-		uint8_t utf8_char;
-		
-		//get first byte - first 4 bit 00000000 00000000 11110000 00000000
-		//and mask with 11100000 
-		utf8_char = ((utf32_char & 0xF000) >> 12) | 0xE0;
-		if (callback)
-			if(callback(user_data, utf8_char))
-				return;
-
-		//get second - 00000000 00000000 00001111 11000000
-		//and mask with 10000000 
-		utf8_char = ((utf32_char & 0x0FC0) >> 6 ) | 0x80;
-		if (callback)
-			if(callback(user_data, utf8_char))
-				return;
-		
-		//get last - 00000000 00000000 00000000 00111111
-		//and mask with 10000000 
-		utf8_char = ( utf32_char & 0x3F)          | 0x80;
-		if (callback)
-			if(callback(user_data, utf8_char))
-				return;
-	}
-	//more than 000000000 00000000 00000000 1000000
-	else if (utf32_char > 0x80){ //2-byte
-		uint8_t utf8_char;
-		//get first byte - first 5 bit 00000000 00000000 00000111 11000000
-		//and mask with 11000000 
-		utf8_char = ((utf32_char & 0x7C0)>> 6) | 0xC0;
-		if (callback)
-			if(callback(user_data, utf8_char))
-				return;
-
-		//get last - 00000000 00000000 00000000 00111111 
-		//and mask with 10000000 
-		utf8_char = ( utf32_char & 0x3F)       | 0x80;
-		if (callback)
-			if(callback(user_data, utf8_char))
-				return;
-	}
-	else { //ANSY
-		uint8_t utf8_char = utf32_char;
-		if (callback)
-			if(callback(user_data, utf8_char))
-				return;
-	}
+		return 0;
+	return 1;
 }
 
-int _utf8rtf_parse_rtf_cb(void *user_data, uint8_t utf8_char){
-	fprintf(stdout, "%c", utf8_char);
+static int _utf8rtf_readword(FILE *fp, char *buf)
+{
+	int ch = 1;
+	int blen = 0;
+	while (1)
+	{
+		ch = fgetc(fp);
+		if (ch == EOF)
+			break;
+		if (_utf8rtf_isinword(ch))
+			buf[blen++] = ch;
+		else
+			break;;
+	}
+	buf[blen] = 0;
+	return ch;
+}
+
+static int _utf8rtf_iscontrol(char *buf)
+{
+	if (
+			(buf[0] >= 'a' && buf[0] <= 'z')
+		 )
+		return 1;
 	return 0;
 }
 
-int _utf8rtf_parse_rtf(FILE *fp)
+static int _utf8rtf_isutf(char *buf)
 {
-	char buf[BUFSIZ], ch;
-	int i, l=0, c;
+	if (buf[0] == 'u')
+		if (buf[1] >= '0' && buf[1] <= '9') 
+			return 1;
+	return 0;
+}
 
-	while ((c=fgetc(fp))!=EOF){
+static int _utf8rtf_is8bit(char *buf)
+{
+	if (buf[0] == '\'')
+		if (
+			(buf[1] >= '0' && buf[1] <= '9') ||
+			(buf[1] >= 'A' && buf[1] <= 'Z') ||
+			(buf[1] >= 'a' && buf[1] <= 'z')
+			)
+		return 1;
+	return 0;
+}
+
+static int _utf8rtf_parse_rtf(FILE *fp)
+{
+	char buf[BUFSIZ];
+	int ch;
+
+	while ((ch=fgetc(fp))!=EOF){
 
 		parse:
-	
-		/* buffer overload */
-		if (l>BUFSIZ-1){
-			perror("buffer overload");
-			break;
-		}
 
-		ch = (char)c;
+		if (ch == '\\'){ // starts with dash
+			char buf[BUFSIZ];	
+			ch = _utf8rtf_readword(fp, buf);
 
-		if (ch == 0x5c){ // dash '\'
-			buf[l++] = ch; 
-			/* look next */
-			c=fgetc(fp); if (c==EOF) break;
-			ch=(char)c;
-			buf[l++] = ch; 
-			if (c== '\''){
-				// this may by 8-bit code
-				c=fgetc(fp); if (c==EOF) break;
-				ch=(char)c;
-				if (ch >= '0' && ch <= '9'){
-					/* this is 8-bit! */
-					char bit8[5];
-					bit8[0] = ch;
-					bit8[1] = fgetc(fp);
-					// todo - handle 8-bit code
+			// check if not service word
+			if (!_utf8rtf_iscontrol(buf)){
+				if (_utf8rtf_is8bit(buf)){
+					// handle with codepages
+					/* TODO: CODEPAGES */
+				} else{
+					// print it
+					fprintf(stdout, "\\%s", buf);
 				}
+				goto parse;
 			}
-			else if (ch == 'u'){
-				/* this may be unicode, look next */	
-				c=fgetc(fp); if (c==EOF) break;
-				ch=(char)c;
-				if (ch >= '0' && ch <= '9'){
-					/* this is UNICODE! */
-					char unicode[5];
-					unicode[0] = ch;
-					unicode[1] = fgetc(fp);
-					unicode[2] = fgetc(fp);
-					unicode[3] = fgetc(fp);
-					unicode[4] = 0;
-
-					uint32_t u;
-					sscanf(unicode, "%u", &u);			
-					_utf32_to_utf8(u, NULL, _utf8rtf_parse_rtf_cb);					
-
-					/* free buffer */
-					for (i = 0; i < l; ++i)
-						buf[i] = 0;	
-					l = 0;					
-
-					/* remove space after unicode */ 
-					c=fgetc(fp); if (c==EOF) break;
-					ch=(char)c;
-					if (ch != ' ')
-						goto parse;
-				}
-				else {
-					buf[l++] = ch; 
-					/* print and free buffer */
-					for (i = 0; i < l; ++i){
-						fprintf(stdout, "%c", buf[i]);
-						buf[i] = 0;	
-					}
-					l = 0;
-				}
+			
+			if (_utf8rtf_isutf(buf)){
+				// handle utf
+				char unicode[5] = 
+				{
+					buf[1], 
+					buf[2], 
+					buf[3], 
+					buf[4], 
+					0
+				};
+				
+				uint32_t u;
+				sscanf(unicode, "%u", &u);			
+				char s[7];
+				int l = _utf8rtf_c32tomb(s, u);
+				s[l] = 0;
+				fprintf(stdout, "%s", s);
+				
+				// if next char is space or ? - drop it
+				if (ch == ' ' || ch == '?')
+					continue;
+			
+				goto parse;
 			}
-			else {
-				/* print and free buffer */
-				for (i = 0; i < l; ++i){
-					fprintf(stdout, "%c", buf[i]);
-					buf[i] = 0;	
-				}
-				l = 0;
-			}
+			
+			/* print service word */
+			fprintf(stdout, "\\%s", buf);
+			goto parse;
 		}
-		else {
-			/* print char */
-			fprintf(stdout, "%c", ch);
-		}
+		
+		// by default print char
+		fprintf(stdout, "%c", ch);
 	}
-
 
 	/* error handling */
 	if (ferror(fp) != 0){
@@ -299,82 +265,84 @@ int utf8rtf_decode(){
 	return _utf8rtf_parse_rtf(stdin);
 }
 
-void 
-_utf8rtf_parse_utf8_cb(void * user_data, uint32_t utf32_char){
-	/* print \uXXXX code with space */
-	fprintf(stdout, "\\u%d ", utf32_char);
-}
-
-int _utf8rtf_parse_utf8(FILE *fp)
+static int _utf8rtf_parse_utf8(FILE *fp)
 {
-	char buf[BUFSIZ], ch;
+	char buf[BUFSIZ];
+	uint8_t ch;
 	int i, l=0, c;
 
 	while ((c=fgetc(fp))!=EOF){
 
-		parse:
-	
-		/* buffer overload */
-		if (l>BUFSIZ-1){
-			perror("buffer overload");
-			break;
-		}
-
 		ch = c;
 
-		if (ch > 128 || ch < 0){ 
-			/* this is utf8 */
-			buf[l++] = ch; 
-			/* get number of bytes in utf8 char */
-			int count = 1;
-			uint8_t u = ch;
-			// 0b11110000
-			if ((u & 0xf0) == 0xf0){ //4
-				count = 4; 
-				for (i = 0; i < 3; ++i) {
-					c=fgetc(fp); if (c==EOF) break;
-					ch=c;
-					buf[l++] = ch; 
-				}
-			}
-			// 0b11100000
-			else if ((u & 0xe0) == 0xe0){ //3
-				count = 3; 
-				for (i = 0; i < 2; ++i) {
-					c=fgetc(fp); if (c==EOF) break;
-					ch=c;
-					buf[l++] = ch; 
-				}
-			}
-			// 0b11000000
-			else if ((u & 0xc0) == 0xc0){ //2
-				count = 2; 
-				c=fgetc(fp); if (c==EOF) break;
-				ch=c;
-				buf[l++] = ch;
-			}			
-			else { //1
-				/* just print char to stdout */	
-				fprintf(stdout, "%c", ch);
-				buf[0] = 0; l = 0;	
-				continue;
-			}
+		if (c >= 252){/* 6-bytes */
+			char buf[7];
+			buf[0] = ch;
+			buf[1] = fgetc(fp);
+			buf[2] = fgetc(fp);
+			buf[3] = fgetc(fp);
+			buf[4] = fgetc(fp);
+			buf[5] = fgetc(fp);
+			buf[6] = 0;
 
-			/* parse buffer */
-			_utf8_to_utf32(count, (uint8_t *)buf, NULL, _utf8rtf_parse_utf8_cb);
-			
-			/* free buffer */
-			for (i = 0; i < l; ++i)
-				buf[i] = 0;	
-			l = 0;
+			uint32_t s32;
+			if (_utf8rtf_mbtoc32(&s32, buf))
+				fprintf(stdout,
+							"\\u%d ", s32);
+		} 
+		else if (c >= 248){/* 5-bytes */
+			char buf[6];
+			buf[0] = ch;
+			buf[1] = fgetc(fp);
+			buf[2] = fgetc(fp);
+			buf[3] = fgetc(fp);
+			buf[4] = fgetc(fp);
+			buf[5] = 0;
+
+			uint32_t s32;
+			if (_utf8rtf_mbtoc32(&s32, buf))
+				fprintf(stdout,
+							"\\u%d ", s32);
 		}
-		else {
-			/* print double space */
-			if (ch == ' ')
-				fprintf(stdout, "%c", ch);
-			/* print char */
+		else if (c >= 240){/* 4-bytes */
+			char buf[5];
+			buf[0] = ch;
+			buf[1] = fgetc(fp);
+			buf[2] = fgetc(fp);
+			buf[3] = fgetc(fp);
+			buf[4] = 0;
+
+			uint32_t s32;
+			if (_utf8rtf_mbtoc32(&s32, buf))
+				fprintf(stdout,
+							"\\u%d ", s32);
+		} 
+		else if (c >= 224){/* 3-bytes */
+			char buf[4];
+			buf[0] = ch;
+			buf[1] = fgetc(fp);
+			buf[2] = fgetc(fp);
+			buf[3] = 0;
+
+			uint32_t s32;
+			if (_utf8rtf_mbtoc32(&s32, buf))
+				fprintf(stdout,
+							"\\u%d ", s32);
+		}
+		else if (c >= 192){/* 2-bytes */
+			char buf[3];
+			buf[0] = ch;
+			buf[1] = fgetc(fp);
+			buf[2] = 0;
+
+			uint32_t s32;
+			if (_utf8rtf_mbtoc32(&s32, buf))
+				fprintf(stdout,
+							"\\u%d ", s32);
+		} 
+		else{/* 1-byte */
 			fprintf(stdout, "%c", ch);
-		}
+		} 
 	}
 
 	/* error handling */
